0d9f930218130ed7a5b2b810671df61ea9ebeae0
KepychCekcu <kepychcekcu@seznam.cz>
2023-08-01 18:59:21 +0200

    video changed

---
+++ kepychcekcu/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var audio = document.getElementById("audioPlayer");
+  audio.autoplay = true;
+  audio.loop = true;
+  audio.play();
+});
