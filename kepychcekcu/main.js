887b4e6a51e710a6a25de0b177bee166f9439f5a
KepychCekcu <kepychcekcu@seznam.cz>
2023-08-01 19:44:12 +0200

    commited sometgin

--- kepychcekcu/main.js
+++ kepychcekcu/main.js
@@ -4,3 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
   audio.loop = true;
   audio.play();
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Získáme stávající počet návštěv z LocalStorage
+  let visitCount = localStorage.getItem("visitCount");
+
+  // Pokud ještě nebyl počet návštěv nastaven, začneme s nulou
+  if (!visitCount) {
+    visitCount = 0;
+  }
+
+  // Aktualizujeme zobrazení počtu návštěv
+  const counterElement = document.getElementById("counter");
+  counterElement.textContent = `Počet návštěv: ${visitCount}`;
+
+  // Zvýšíme počet návštěv o 1 a uložíme ho zpět do LocalStorage
+  visitCount++;
+  localStorage.setItem("visitCount", visitCount.toString());
+});
